a9232fb9a6ebb65a2f023e40260c78a4e39723fd
Adrian-ok <vargasadrianok@gmail.com>
2023-06-02 08:35:28 -0300

    add no result message

--- Projects/00-React-router-practice/src/hooks/useGetDate.js
+++ Projects/00-React-router-practice/src/hooks/useGetDate.js
@@ -3,18 +3,22 @@ import { getDateDetails } from "../service/FetchDate";
 
 export function useGetDate() {
     
-    const [pick, setPick] = useState()
+    const [pick, setPick] = useState(null)
+    const [msjempty, setMsjempty] = useState(false)
     const [dates, setDates] = useState([])
     const [loading, setLoading] = useState(false)
 
     const findDate = () => {
         
         setLoading(true)
+        setMsjempty(false)
 
         const storeDates = localStorage.getItem('fechas')
+        const storePick = localStorage.getItem('date')
 
-        if (storeDates) {
+        if (storeDates && storePick) {
             setDates(JSON.parse(storeDates))
+            setPick(JSON.parse(storePick))
             setLoading(false)
         }
         else {
@@ -22,12 +26,15 @@ export function useGetDate() {
 
                 getDateDetails(pick)
                     .then(newState => {
-                        setDates(newState);
-                        setLoading(false);
-                        localStorage.setItem('fechas', JSON.stringify(newState));
+                        setDates(newState)
+                        setLoading(false)
+                        localStorage.setItem('fechas', JSON.stringify(newState))
+                        localStorage.setItem('date', JSON.stringify(pick) )
+                        if(newState.length === 0) {setMsjempty(true)}
                     })
                     .catch(error => {
-                        setLoading(false);
+                        setLoading(false)
+                        setMsjempty(false)
                     });
             }
             else {
@@ -38,5 +45,5 @@ export function useGetDate() {
 
     useEffect(findDate, [pick])
 
-    return {dates, setPick, loading}
+    return {dates, pick, msjempty, setPick, loading}
 }
--- Projects/00-React-router-practice/src/pages/FindDate.jsx
+++ Projects/00-React-router-practice/src/pages/FindDate.jsx
@@ -4,7 +4,7 @@ import { useGetDate } from '../hooks/useGetDate'
 
 export function FindDate() {
 
-    const { dates, setPick, loading } = useGetDate()
+    const { dates, pick, msjempty, setPick, loading } = useGetDate()
 
     const pickDate = (event) => {
         localStorage.removeItem('fechas')
@@ -15,13 +15,12 @@ export function FindDate() {
     return (
         <main className='max-w-screen-lg mx-auto mt-12'>
             <div className='flex justify-center my-10'>
-                <input className='p-2 rounded-md w-48' type='date' onChange={pickDate} />
+                <input className='p-2 rounded-md w-48' type='date' onChange={pickDate} value={pick}/>
             </div>
 
-            {
-                loading ?
-                    <p className='text-white text-4xl text-center'>Cargando...</p> :
-                    dates != undefined && <Solicitudes items={dates} />
+            { 
+                loading ? <p className='text-white text-4xl text-center'>Cargando...</p> : 
+                (msjempty ? <p className='text-white text-2xl text-center'>No hay resultados</p> : <Solicitudes items={dates}/>)
             }
 
         </main>
