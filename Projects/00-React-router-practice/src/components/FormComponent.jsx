ebbd78477e504b6d49b83e83bd6681c76ab7f55e
Adrian-ok <vargasadrianok@gmail.com>
2023-06-05 12:24:11 -0300

    add styling component

--- Projects/00-React-router-practice/src/components/FormComponent.jsx
+++ Projects/00-React-router-practice/src/components/FormComponent.jsx
@@ -29,41 +29,86 @@ export function FormComponent() {
     return (
         <>
             {loading ? <p className='text-white text-4xl text-center'>Cargando...</p> :
-                <form className='flex flex-col justify-center items-center gap-2' onSubmit={sendQuery}>
-                    <h4>Nueva Solicitud</h4>
+                <div className="max-w-md mx-auto bg-gray-300 p-6 rounded-xl mt-12">
+                    <form className="flex flex-col justify-center items-center gap-2">
+                        <h4 className="text-center font-bold">Nueva Solicitud</h4>
 
-                    <select onChange={pickOption} className='rounded-md p-1' defaultValue='default' name='sector'>
-                        <option value='default'>Selecione Sector</option>
-                        {SECTORES.map(item => {
-                            return (
-                                <option key={item.value} value={item.sector}>{item.sector}</option>
-                            )
-                        })}
-                    </select>
+                        <select
+                            onChange={pickOption}
+                            className="w-full rounded-md p-1 h-10"
+                            defaultValue="default"
+                            name="sector"
+                        >
+                            <option value="default">Selecciona un Sector</option>
+                            {SECTORES.map((item) => {
+                                return (
+                                    <option key={item.value} value={item.sector}>
+                                        {item.sector}
+                                    </option>
+                                );
+                            })}
+                        </select>
 
-                    <select className='rounded-md p-1' defaultValue='default' name='maquina'>
-                        <option value='default'>Selecione Maquina</option>
-                        {machine.map(item => {
-                            return (
-                                <option key={item.id} value={item.nombre}>{item.nombre}</option>
-                            )
-                        })}
-                    </select>
+                        <select
+                            className="w-full rounded-md p-1 h-10"
+                            defaultValue="default"
+                            name="maquina"
+                        >
+                            <option value="default">Selecciona una Máquina</option>
+                            {machine.map((item) => {
+                                return (
+                                    <option key={item.id} value={item.nombre}>
+                                        {item.nombre}
+                                    </option>
+                                );
+                            })}
+                        </select>
 
-                    <textarea type="text" placeholder='Detalle' maxLength='250' name='detalle' required />
+                        <textarea
+                            type="text"
+                            placeholder="Detalle"
+                            maxLength="250"
+                            name="detalle"
+                            required
+                            className="w-full rounded-md p-1"
+                        />
 
-                    <div>
-                        <input type="radio" name='opt' value='Normal' checked={checked === 'Normal'} onChange={changeChecked} />
-                        <label>Normal</label>
-                        <input type="radio" name='opt' value='Urgente' checked={checked === 'Urgente'} onChange={changeChecked} />
-                        <label>Urgente</label>
-                        <input type="radio" name='opt' value='Critico' checked={checked === 'Critico'} onChange={changeChecked} />
-                        <label>Critico</label>
-                    </div>
+                        <div className="flex flex-col">
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="opt"
+                                    value="Normal"
+                                    checked={checked === "Normal"}
+                                    onChange={changeChecked}
+                                />
+                                Normal
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="opt"
+                                    value="Urgente"
+                                    checked={checked === "Urgente"}
+                                    onChange={changeChecked}
+                                />
+                                Urgente
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="opt"
+                                    value="Critico"
+                                    checked={checked === "Critico"}
+                                    onChange={changeChecked}
+                                />
+                                Crítico
+                            </label>
+                        </div>
 
-                    <button className='bg-blue-400 rounded-sm p-1'>Enviar</button>
-
-                </form>
+                        <button className="w-full bg-blue-400 rounded-sm p-1">Enviar</button>
+                    </form>
+                </div>
             }
         </>
     )
