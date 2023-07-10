c2bad8ca453fedda3b0f0ef195a470c711224078
Adrian-ok <vargasadrianok@gmail.com>
2023-07-10 10:17:16 -0300

    add styles

--- Projects/02-Expense-tracker/src/App.jsx
+++ Projects/02-Expense-tracker/src/App.jsx
@@ -3,17 +3,18 @@ import { Balance } from './components/Balance'
 import { FormTransaction } from './components/FormTransaction'
 import { TransactionItem } from './components/transactions/TransactionItem'
 import { IncomeExpenses } from './components/IncomeExpenses'
+import Container from './components/Container'
 
 function App() {
 
   return (
-    <>
+    <Container className='justify-center h-screen bg-slate-600 '>
       <Header />
       <IncomeExpenses/>
       <Balance />
       <FormTransaction />
       <TransactionItem />
-    </>
+    </Container>
   )
 }
 
--- Projects/02-Expense-tracker/src/components/Header.jsx
+++ Projects/02-Expense-tracker/src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export function Header() {
   return (
-    <div>Header</div>
+    <div className='mx-auto max-w-6xl px-2 py-8 sm:px-6 sm:py-6 lg:px-8 rounded-md bg-blue-200 dark:bg-blue-900 dark:text-white'>
+      header
+    </div>
   )
 }
